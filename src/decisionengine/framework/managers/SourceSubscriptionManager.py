1a0685ce6e80c616de1634db0dccfbba2aa64c35
Brandon White <bjwhite@fnal.gov>
2021-04-15 13:34:01 -0500

    SourceSubscriptionManager starts and stops properly now.

--- src/decisionengine/framework/managers/SourceSubscriptionManager.py
+++ src/decisionengine/framework/managers/SourceSubscriptionManager.py
@@ -3,6 +3,7 @@ Source Subscription Manager
 """
 
 import time
+import queue
 
 # The manager will run in a thread
 import threading
@@ -16,18 +17,18 @@ class SourceSubscriptionManager(threading.Thread):
     """
     def __init__(self):
         super().__init__()
+        current_manager = multiprocessing.Manager()
+        self.current_t0_data_blocks = current_manager.dict()
+
         self.keep_running = multiprocessing.Value('i', 1)
-        self.message_queue = multiprocessing.Queue()
-
-    def subscribe_to_source(self):
-        pass
-
-    def unsubscribe_from_source(self):
-        pass
+        self.data_block_queue = multiprocessing.Queue()
 
     def run(self):
         while self.keep_running.value:
-            # listen for updated data product
-            # send data products to subscribers
-            time.sleep(5)
+            try:
+                source_new_data = self.data_block_queue.get(timeout=1)
+            except Queue.Empty:
+                pass
+            source, new_t0_data_block = source_new_data
+            self.current_t0_data_blocks[source] = new_t0_data_block
         self.logger.info('Source Subscription Manager is stopping.')
